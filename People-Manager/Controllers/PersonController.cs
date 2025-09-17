3edad37c41b6cfa1f3fa704c563b63ec34c20960
Fabiovgc <fabiovgc@gmail.com>
2025-09-17 13:09:27 -0300

    feat: add 'PersonController' returnin a View

---
+++ People-Manager/Controllers/PersonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace People_Manager.Controllers
+{
+    [Route("User")]
+    public class PersonController : Controller
+    {
+        [Route("start")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
