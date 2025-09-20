f0e58b790291f079801b6b8fdaa176a972574c31
Fabiovgc <fabiovgc@gmail.com>
2025-09-19 23:50:13 -0300

    feat(controller): add metadata to views and refactor PersonDetails method
    - Added ViewBag.TextoDescricao to provide a description text for the view
    - Added ViewData['DataAtual'] to pass the current date/time to the view
    - Refactored PersonDetails(int Id) to:
      - Load a list of predefined people
      - Use FirstOrDefault to return the person matching the requested Id
      - Return 404 (NotFound) when the Id does not exist

--- People-Manager/Controllers/PeopleController.cs
+++ People-Manager/Controllers/PeopleController.cs
@@ -6,10 +6,16 @@ namespace People_Manager.Controllers
     [Route("User")]
     public class PeopleController : Controller
     {
+
+
+
+
         [Route("start")]
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/Person/Index.cshtml");
+            
+            //TempData["SucessoRedirecionamento"] = "O redirecionamento foi um sucesso";
         }
 
 
@@ -17,6 +23,14 @@ namespace People_Manager.Controllers
         [Route("details")]
         public IActionResult PeopleDetails()
         {
+            
+            // ViewBag is a dynamic object that allows you to pass data from the controller to the view
+            ViewBag.TextoDescricao = "Texto da tela de descrição";
+
+            ViewData["DataAtual"] = DateTime.Now;
+
+            // TempData["SucessoRedirecionamento"] = "O redirecionamento foi um sucesso";
+
             var people = new List<Person>();
             people.Add(new Person(1, "Fabio", "Colonese", DateTime.Now));
             people.Add(new Person(2, "Mario", "Rossi", DateTime.Now));
@@ -24,6 +38,8 @@ namespace People_Manager.Controllers
             people.Add(new Person(4, "Anna", "Bianchi", DateTime.Now));
 
             return View(people);
+
+            //return RedirectToAction("Index");
         }
 
 
@@ -31,8 +47,32 @@ namespace People_Manager.Controllers
         [Route("details/{Id:int}")]
         public IActionResult PersonDetails(int Id)
         {
-            var person = new Person(1, "Fabio", "Colonese", DateTime.Now);
-            return View(person);
+            ViewBag.TextoDescricao  = "Texto da tela de descrição";
+
+            ViewData["DataAtual"] = DateTime.Now;
+
+            //TempData["SucessoRedirecionamento"] = "O redirecionamento foi um sucesso";
+
+            var people = new List<Person>
+            {
+                new Person(1, "Fabio", "Colonese", DateTime.Now),
+                new Person(2, "Mario", "Rossi", DateTime.Now),
+                new Person(3, "Luigi", "Verdi", DateTime.Now),
+                new Person(4, "Anna", "Bianchi", DateTime.Now)
+            };
+
+            var person = people.FirstOrDefault(p => p.Id == Id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            //var person = new Person(1, "Fabio", "Colonese", DateTime.Now);
+            
+            return View("~/Views/Person/PersonDetails.cshtml", person);
+            
+            //return RedirectToAction("Index");
         }
 
 
