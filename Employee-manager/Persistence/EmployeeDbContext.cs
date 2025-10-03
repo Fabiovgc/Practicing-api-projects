0d92e0966efc2af0678f5836ac468c95c580919e
Fabiovgc <fabiovgc@gmail.com>
2025-10-03 10:51:24 -0300

    feat: add EmployeeDbContext with DbSet<Employee> and model configuration

---
+++ Employee-manager/Persistence/EmployeeDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Employee_manager.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_manager.Persistence
+{
+    public class EmployeeDbContext : DbContext
+    {
+       public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Employee>(o =>
+            {
+                o.HasKey(j => j.Id);
+                // other configurations can be added here
+
+            });
+        }
+
+    }
+}
