a858c81c5093cb12670607fd5b59b8faf7958902
Fabiovgc <fabiovgc@gmail.com>
2025-10-06 11:23:25 -0300

    test: testing synchronization

--- Employee-manager/Program.cs
+++ Employee-manager/Program.cs
@@ -13,7 +13,7 @@ namespace Employee_manager
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
