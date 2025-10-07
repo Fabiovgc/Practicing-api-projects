3ab66b5dd6850886c260c8101c7d52eace1c1fbe
Fabiovgc <fabiovgc@gmail.com>
2025-10-07 13:52:27 -0300

    feat: add 'EventsDbContext' and Id configuration

---
+++ Events-manager/Persistency/EventsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Events_manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events_manager.NewFolder
+{
+    public class EventsDbContext : DbContext
+    {
+
+        
+        public EventsDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+
+
+        //configuration
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // haskey
+            builder.Entity<Event>(o =>
+            {
+                o.HasKey(e => e.Id);
+
+            });
+        }
+    }
+}
